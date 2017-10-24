c1f2b3a0d1481085ae48d2f44492e7f57565cb0e
SirSalerno <bruno@mba.local>
2017-10-24 10:05:07 -0200

    actually searching for seven zeros

--- geraHash.py
+++ geraHash.py
@@ -4,12 +4,12 @@ import random
 
 def id_generator(size, chars=string.ascii_letters + string.digits + string.punctuation):
  return ''.join(random.choice(chars) for _ in range(size))
- 
+
 while (True):
   numero_da_sorte=random.randint(10,50)	
   s=id_generator(numero_da_sorte)
   obj = sha1(s)
   aux=obj.hexdigest()
-  if (aux.endswith('0'*8)):
+  if (aux.endswith('0'*7)):
     break
 print "Hash: " + aux + " Entrada: " + s
